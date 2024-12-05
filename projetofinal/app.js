d5cab874f0dd0dfb73a77bf1a141c22bd7598267
Simao-dev <pedrosimaocontato@gmail.com>
2024-12-05 15:49:40 -0300

    versão 1.0 game show

--- projetofinal/app.js
+++ projetofinal/app.js
@@ -1,32 +1,23 @@
-let chute = document.querySelector("#send")
 let numeroMaximo = 100;
-let numeroSecreto = parseInt(Math.random() * numeroMaximo  + 1);
+let numeroSecreto = parseInt(Math.random() * numeroMaximo + 1);
 let tentativa = 1;
-var title = document.querySelector("#resultado")
+let title = document.querySelector("#resultado");
+let chute = document.querySelector("#send");
 
-chute.addEventListener("click", function(e){
-    
+chute.addEventListener("click", function(e) {
     e.preventDefault();
 
-    let name = document.querySelector("#dados");
+    const name = document.querySelector("#dados");
+    const value = parseInt(name.value); // Certifique-se que o valor seja um número
 
-    let value = name.value;
-
-});
-
-while ( value != numeroSecreto){
-    
-    if(value == numeroSecreto){
-        break
-    }else {
-        if (value > numeroSecreto){
-            title.innerHTML = `E menor que ${value}`
-        }else {
-            title.innerHTML = `E maior que ${value}`
+    if (value === numeroSecreto) {
+        title.innerHTML = `Você descobriu o número secreto! ${numeroSecreto} em ${tentativa} ${tentativa > 1 ? "tentativas" : "tentativa"}`;
+    } else {
+        tentativa++;
+        if (value > numeroSecreto) {
+            title.innerHTML = `O número secreto é menor que ${value}`;
+        } else {
+            title.innerHTML = `O número secreto é maior que ${value}`;
         }
-        tentativa++
     }
-}
-
-let palavraTentativa = tentativa > 1 ? "tentativas" : "tentativa";
-title.innerHTML = `Você descobriu o número secreto! ${numeroSecreto} com ${tentativa} ${palavraTentativa}`
+});
