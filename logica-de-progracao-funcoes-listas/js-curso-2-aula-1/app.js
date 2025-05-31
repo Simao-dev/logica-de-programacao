dd41dbf0269b30d574495b700abecbf2e8789b88
Simao-dev <pedrosimaocontato@gmail.com>
2025-05-30 21:10:29 -0300

    iniciado os desafios

--- logica-de-progracao-funcoes-listas/js-curso-2-aula-1/app.js
+++ logica-de-progracao-funcoes-listas/js-curso-2-aula-1/app.js
@@ -10,10 +10,26 @@ exibirTestoNaTela('h1', 'Jogo do numero secreto');
 exibirTestoNaTela('p', 'Escolha um numero entre 1 e 10');
 
 function verificarChute() {
-    console.log(numeroSecreto);
+    let numero = document.querySelector('input').value;
+    console.log(numeroSecreto == numero);
 }
 
 function gerarNumeroAleatorio () {
     
     return parseInt(Math.random() * 10 + 1);
 }
+
+
+
+function saudacao() {
+  return("Olá, mundo");
+}
+
+console.log (saudacao())
+
+
+function comprimentar (luiza) {
+    return(`ola ${luiza}`);
+}
+
+console.log(comprimentar())
